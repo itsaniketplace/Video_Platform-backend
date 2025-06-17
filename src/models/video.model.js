be46fc6cc12f4f010657ef10ad8197ca880f6d09
itsaniketplace <aniyadav1105@gmail.com>
2025-06-17 15:33:43 +0530

    add user and video models

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import mongooseAggrigatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videofile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    Description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudinary url
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    ispublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggrigatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
